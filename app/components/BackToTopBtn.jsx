ad07b59abc0f6afea070586a726b863ee83e7e12
MartinaMax <mmaximova37@gmail.com>
2024-12-02 16:00:18 +0100

    Back to top button component

---
+++ app/components/BackToTopBtn.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const BackToTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show the button when page is scrolled down
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 1000) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    isVisible && (
+      <button onClick={scrollToTop}>
+        <img src="svg/UpArrow.svg" alt="Up arrow svg"/>
+      </button>
+    )
+  );
+};
+
+export default BackToTopBtn;
