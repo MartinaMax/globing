ca8a2e824a4803a7a852093274adaafe0b80c479
MartinaMax <mmaximova37@gmail.com>
2024-12-02 20:44:09 +0100

    Phone number contact section

---
+++ app/components/PhoneNmbrContact.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+const PhoneNmbrContact = () => {
+
+  return (
+    <div>
+        <div>
+            <h2>Potrebujete vyhotoviť geometrický plán alebo vytýčiť hranicu pozemku?</h2>
+            <h2>Ing. Vladimír Kochan 0905273309</h2>
+            <h2>Ing. Juraj Kubičár 0905273310</h2>
+        </div>
+        <div>
+            <h2>Potrebujete vytýčiť stavbu, polohopisné a výškopisné zameranie či iné prace v oblasti inžinierskej geodézie? </h2>
+            <h2>Ing. Vlastimil Maxim 0905273312</h2>
+        </div>
+        <div>
+            <h2>Potrebujete vyhotoviť projekt pozemkových úprav?</h2>
+            <h2>Ing. Juraj Kubičár 0905273310</h2>
+        </div>
+    </div>
+  );
+};
+
+export default PhoneNmbrContact;
