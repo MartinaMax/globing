323dc3736a6f732c135fda60adc64e130655c2a0
MartinaMax <mmaximova37@gmail.com>
2024-12-03 18:32:49 +0100

    Reference list component and some style changes

--- app/components/BackToTopBtn.jsx
+++ app/components/BackToTopBtn.jsx
@@ -9,31 +9,31 @@ const BackToTopBtn = () => {
     // Show the button when page is scrolled down
     useEffect(() => {
         const toggleVisibility = () => {
-        if (window.scrollY > 1000) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+            if (window.scrollY > 1000) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
         };
 
         window.addEventListener("scroll", toggleVisibility);
         return () => {
-        window.removeEventListener("scroll", toggleVisibility);
+            window.removeEventListener("scroll", toggleVisibility);
         };
     }, []);
 
     const scrollToTop = () => {
         window.scrollTo({
-        top: 0,
-        behavior: "smooth",
+            top: 0,
+            behavior: "smooth",
         });
     };
 
     return (
         isVisible && (
-        <button onClick={scrollToTop}>
-            <img src="svg/arrow-up.svg" alt="Up arrow svg"/>
-        </button>
+            <button onClick={scrollToTop}>
+                <img src="svg/arrow-up.svg" alt="Up arrow svg"/>
+            </button>
         )
     );
 };
