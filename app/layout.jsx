b12d4805895989c0bdfcbe97202d69a4cf23ffae
MartinaMax <mmaximova37@gmail.com>
2024-12-15 23:34:34 +0100

    Added meta description for better practice

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import variables from '@/styles/variables.module.scss'
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <meta name="description" content="" />
+            </head>
             <body className={variables.body} >
                 {children}
             </body>
