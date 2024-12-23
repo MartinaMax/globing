05974ab52fa0f9ab75a3a764b766de080b7bbceb
MartinaMax <mmaximova37@gmail.com>
2024-12-23 12:28:16 +0100

    Head updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,11 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <meta name="description" content="" />
+                {/* Favicon link */}
+                <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+                <title>GLOBING POPRAD s.r.o</title>
+                {/* Meta description */}
+                <meta name="description" content="GLOBING POPRAD s.r.o offers a wide range of services in the field of geodesy and cartography. Our years of experience and qualified team guarantee professionally executed work." />
             </head>
             <body className={variables.body} >
                 {children}
